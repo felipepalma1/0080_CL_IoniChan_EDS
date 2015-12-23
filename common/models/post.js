ac60f860350841057b03db13d3fd025aac4df40f
dema1348 <eds.perez@outlook.com>
2015-12-23 11:10:53 -0300

    fix

--- common/models/post.js
+++ common/models/post.js
@@ -3,9 +3,7 @@ module.exports = function(Post) {
 
 	Post.observe('before save', function(ctx, next) {
 	var Comentario = ctx.Model.app.models.Comentario;
-
     if (ctx.instance) {
-
 		Post.count(function (err,data) {
 			    ctx.instance.enumeracion=data;
 			    ctx.instance.fecha= Date.now();
@@ -14,16 +12,11 @@ module.exports = function(Post) {
 					console.log(ctx.instance);
 					next();
 			});
-			
 		});
-
-		
     }
-
     else{
     	next();
     }
-
     
   });	
 };
